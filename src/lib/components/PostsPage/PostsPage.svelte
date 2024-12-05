4b30cba5f5625c8e3608af79a8eada4a623a4be6
vamaridev <vamaridev@gmail.com>
2024-12-05 19:26:32 +0200

    add: адаптивная страница с постами

--- src/lib/components/PostsPage/PostsPage.svelte
+++ src/lib/components/PostsPage/PostsPage.svelte
@@ -1,1 +1,67 @@
-<h1>PostsPage.svelte</h1>
+<div class="mx-auto max-w-[1440px] bg-lightBackground">
+<div class="flex mx-auto py-16 px-16 items-center justify-between flex-wrap lg:flex-nowrap">
+  <div class="max-w-[625px] mr-4">
+    <h1 class="font-inter text-base text-primary font-medium leading-5 text-left uppercase">Актуальный Пост</h1>
+    <h2 class="text-3xl text-third font-bold leading-12 mt-3">Пошаговое руководство по выбору идеальных шин для вашего авто</h2>
+    <p class="mt-2 text-sm font-medium text-primary">23 мая, 2024</p>
+    <p class="mt-2 text-base font-normal leading-7 text-textAlt mb-4">Шины — это не просто расходный материал, а важный элемент безопасности и комфорта. Узнайте, как правильно выбрать шины для вашего автомобиля, учитывая сезон, стиль вождения и тип дорог.</p>
+    <button class="rounded-md px-[48px] py-[16px] bg-secondary text-text font-bold no-underline hover:bg-hoverButton transition-bg duration-200 ease-in-out">Подробнее</button>
+   </div>
+    <img class=" max-w-full h-auto sm:max-w-[500px] sm:h-[350px] ml-0 mt-6 lg:mt-0 " src="images/car.jpg" alt="">
+  </div>
+</div>
+
+
+
+<div class="max-w-[1440px] mx-auto mt-20">
+    <div class="px-16">
+<h1 class="text-third text-4xl font-extrabold">Все Посты</h1>
+<div class="border bg-textAlt mt-6"></div>
+<div>
+    <div class=" lg:mx-0 pb-5 lg:pb-0 lg:max-w-none max-w-[600px] lg:flex items-center mt-10 hover:bg-lightBackground cursor-pointer transition transition-colors ">
+        <img class="lg:max-w-[490px] lg:h-[310px] max-w-full h-auto lg:pb-0 pb-4" src="images/car.jpg" alt="">
+        <div class="pl-10 lg:max-w-[625px] mr-2">
+            <h1 class="text-secondary text-[16px] font-medium">#РемонтАвто</h1>
+            <h2 class="text-4xl font-extrabold leading-12 text-third mt-3">Советы по выбору идеальных запчастей для вашего автомобиля</h2>
+            <p class="mt-2 text-sm font-medium text-primary">23 мая, 2024</p>
+            <p class="mt-2 text-base font-normal leading-7 text-textAlt">Выбор запчастей для автомобиля — это ключевой момент в обеспечении его долговечности и безопасности. Узнайте, как выбрать качественные компоненты для своего авто.</p>
+        </div>
+
+    </div>
+    <div class="lg:mx-0 pb-5 lg:pb-0 lg:max-w-none max-w-[600px] lg:flex items-center mt-10 hover:bg-lightBackground cursor-pointer transition transition-colors">
+        <img class="lg:max-w-[490px] lg:h-[310px] max-w-full h-auto lg:pb-0 pb-4" src="images/audi.jpg" alt="">
+        <div class="ml-10 max-w-[625px] mr-2">
+            <h1 class="text-secondary text-[16px] font-medium">#ОбслуживаниеАвто</h1>
+            <h2 class="text-4xl font-extrabold leading-12 text-third mt-3">Как правильно настроить тормоза вашего автомобиля</h2>
+            <p class="mt-2 text-sm font-medium text-primary">23 мая, 2024</p>
+            <p class="mt-2 text-base font-normal leading-7 text-textAlt">Тормоза — одна из самых важных систем безопасности. Разберитесь, как их правильно настроить и какие признаки неисправности могут возникнуть.</p>
+        </div>
+
+    </div>
+    <div class="lg:mx-0 pb-5 lg:pb-0 lg:max-w-none max-w-[600px] lg:flex items-center mt-10 hover:bg-lightBackground cursor-pointer transition transition-colors">
+        <img class="lg:max-w-[490px] lg:h-[310px] max-w-full h-auto lg:pb-0 pb-4" src="images/car.jpg" alt="">
+        <div class="ml-10 max-w-[625px] mr-2">
+            <h1 class="text-secondary text-[16px] font-medium">#Автосервис</h1>
+            <h2 class="text-4xl font-extrabold leading-12 text-third mt-3">Как подготовить авто к зимнему сезону: пошаговое руководство</h2>
+            <p class="mt-2 text-sm font-medium text-primary">23 мая, 2024</p>
+            <p class="mt-2 text-base font-normal leading-7 text-textAlt">Зима — это серьёзное испытание для автомобиля. Узнайте, как подготовить машину к холодам, чтобы избежать неприятных сюрпризов.</p>
+        </div>
+
+    </div>
+    <div class="lg:mx-0 pb-5 lg:pb-0 lg:max-w-none max-w-[600px] lg:flex items-center mt-10 hover:bg-lightBackground cursor-pointer transition transition-colors">
+        <img class="lg:max-w-[490px] lg:h-[310px] max-w-full h-auto lg:pb-0 pb-4" src="images/audi.jpg" alt="">
+        <div class="ml-10 max-w-[625px] mr-2">
+            <h1 class="text-secondary text-[16px] font-medium">#Автосервис</h1>
+            <h2 class="text-4xl font-extrabold leading-12 text-third mt-3">Как выбрать и заменить тормозные колодки для вашего автомобиля</h2>
+            <p class="mt-2 text-sm font-medium text-primary">23 мая, 2024</p>
+            <p class="mt-2 text-base font-normal leading-7 text-textAlt">Тормозные колодки — один из важнейших элементов безопасности автомобиля. В этом посте мы расскажем, как правильно выбрать подходящие тормозные колодки для вашего авто и когда пришло время их заменить. Следите за состоянием тормозной системы, чтобы обеспечить себе безопасную поездку.</p>
+        </div>
+
+    </div>
+
+ 
+    
+</div>
+</div>
+
+</div>

30dc9bf59ed1b20a2d56898f921c093175820e03
vladkuzdev <vladkuzdev@gmail.com>
2025-07-09 22:23:40 +0300

    add: прокрутка карусели

--- src/lib/components/MainPage/Carousel/Carousel.svelte
+++ src/lib/components/MainPage/Carousel/Carousel.svelte
@@ -1,5 +1,6 @@
 <script>
   import './carousel.css'
+  import { onMount, onDestroy } from 'svelte';
 
   import Item1 from "./Items/Item1.svelte";
   import Item2 from "./Items/Item2.svelte";
@@ -13,7 +14,9 @@
   ];
 
   let currentSlideItem = 0;
-  
+  let userInteracted = false;
+  let interval;
+
   const nextItem = () => {
     currentSlideItem = (currentSlideItem + 1) % items.length;
   }
@@ -26,11 +29,33 @@
     }
   }
 
+  const handlePrev = () => {
+    userInteracted = true;
+    prevItem();
+  }
+
+  const handleNext = () => {
+    userInteracted = true;
+    nextItem();
+  }
+
+  onMount(() => {
+    interval = setInterval(() => {
+      if (!userInteracted) {
+        nextItem();
+      }
+    }, 5000);
+  });
+
+  onDestroy(() => {
+    clearInterval(interval);
+  });
+
 </script>
 
 {#each [items[currentSlideItem]] as Item (currentSlideItem)}
     <Item>
-        <button class="left-button absolute left-4  top-1/2 transform -translate-y-1/2 text-text text-4xl bg-black bg-opacity-50 p-4 rounded-full" on:click={() => prevItem()}>&#10094;</button>
-        <button class="right-button absolute right-4 top-1/2 transform -translate-y-1/2 text-text text-4xl bg-black bg-opacity-50 p-4 rounded-full" on:click={() => nextItem()}>&#10095;</button>
+        <button class="left-button absolute left-4  top-1/2 transform -translate-y-1/2 text-text text-4xl bg-black bg-opacity-50 p-4 rounded-full" on:click={handlePrev}>&#10094;</button>
+        <button class="right-button absolute right-4 top-1/2 transform -translate-y-1/2 text-text text-4xl bg-black bg-opacity-50 p-4 rounded-full" on:click={handleNext}>&#10095;</button>
     </Item>
 {/each}

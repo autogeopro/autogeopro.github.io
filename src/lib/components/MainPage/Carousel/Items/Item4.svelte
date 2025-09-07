aa9db4c93209c6de4fa6e028006a4e7d56003a68
vladkuzdev <vladkuzdev@gmail.com>
2025-09-07 22:27:50 +0300

    add: фото о нас в карусель

--- src/lib/components/MainPage/Carousel/Items/Item4.svelte
+++ src/lib/components/MainPage/Carousel/Items/Item4.svelte
@@ -1,6 +1,6 @@
 <div class="main_container relative pt-5 mx-auto max-w-[1440px] pb-5">
     <img class="absolute z-0 top-0 left-0 w-full h-full object-cover
-       transform scale-x-[-1] brightness-125" src="images/audi.jpg" alt="">
+       transform scale-x-[-1] brightness-125" src="images/cars-glav.jpg" alt="">
       
     <div class="main_container_block relative bg-white bg-opacity-50 p-6 mt-5 mb-5 rounded-lg mr-10 ml-14 md:ml-24 max-w-[790px] min-h-[350px] flex flex-col justify-center items-start  bg-gradient-to-r from-[rgba(255,255,255,0.8)] to-transparent">
       <h1 class="main_text_bold font-bold text-[45px] text-third m-0 leading-[56px] xs:leading-[40px] xs:text-[30px]">Дороги ведут к нам!</h1>

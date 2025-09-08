02480bc44ed819ff1a45113ccb5f49817b191d28
vladkuzdev <vladkuzdev@gmail.com>
2025-09-08 22:54:52 +0300

    add:  фото в карусели и скрола карусели

--- src/lib/components/MainPage/Carousel/Carousel.svelte
+++ src/lib/components/MainPage/Carousel/Carousel.svelte
@@ -1,70 +1,74 @@
 <script>
-  import './carousel.css'
+  import './carousel.css';
   import { onMount, onDestroy } from 'svelte';
 
   import Item1 from "./Items/Item1.svelte";
   import Item2 from "./Items/Item2.svelte";
   import Item3 from "./Items/Item3.svelte";
   import Item4 from "./Items/Item4.svelte";
-  const items = [
-    Item1,
-    Item2,
-    Item3,
-    Item4,
-  ];
+
+  const items = [Item1, Item2, Item3, Item4];
 
   let currentSlideItem = 0;
-  let userInteracted = false;
   let interval;
 
   const nextItem = () => {
     currentSlideItem = (currentSlideItem + 1) % items.length;
-  }
+  };
 
   const prevItem = () => {
-    if (currentSlideItem !== 0) {
-        currentSlideItem = (currentSlideItem - 1 + items.length) % items.length;
-    } else {
-        currentSlideItem = items.length - 1;
-    }
-  }
+    currentSlideItem = (currentSlideItem - 1 + items.length) % items.length;
+  };
+
+  const resetAutoplay = () => {
+    clearInterval(interval);
+    interval = setInterval(nextItem, 10000); // каждые 10 секунд
+  };
 
   const handlePrev = () => {
-    userInteracted = true;
     prevItem();
-  }
+    resetAutoplay(); // перезапуск после ручного клика
+  };
 
   const handleNext = () => {
-    userInteracted = true;
     nextItem();
-  }
+    resetAutoplay();
+  };
 
   onMount(() => {
-    interval = setInterval(() => {
-      if (!userInteracted) {
-        nextItem();
-      }
-    }, 5000);
+    resetAutoplay();
   });
 
   onDestroy(() => {
     clearInterval(interval);
   });
-
 </script>
 
 <div class="relative grid">
-    {#each items as Item, i}
-        <div
-            class="col-start-1 row-start-1 transition-opacity duration-500"
-            class:opacity-100={i === currentSlideItem}
-            class:opacity-0={i !== currentSlideItem}
-            class:pointer-events-none={i !== currentSlideItem}
+  {#each items as Item, i}
+    <div
+      class="col-start-1 row-start-1 transition-opacity duration-500"
+      class:opacity-100={i === currentSlideItem}
+      class:opacity-0={i !== currentSlideItem}
+      class:pointer-events-none={i !== currentSlideItem}
+    >
+      <svelte:component this={Item}>
+        <button
+          class="left-button absolute left-4 top-1/2 -translate-y-1/2 text-text text-4xl bg-black/30 hover:bg-black/50 transition-all p-4 rounded-full z-10"
+          on:click={handlePrev}
+          aria-label="Предыдущий слайд"
         >
-            <Item>
-                <button class="left-button absolute left-4 top-1/2 transform -translate-y-1/2 text-text text-4xl bg-black bg-opacity-30 hover:bg-opacity-50 transition-all p-4 rounded-full z-10" on:click={handlePrev}>&#10094;</button>
-                <button class="right-button absolute right-4 top-1/2 transform -translate-y-1/2 text-text text-4xl bg-black bg-opacity-30 hover:bg-opacity-50 transition-all p-4 rounded-full z-10" on:click={handleNext}>&#10095;</button>
-            </Item>
-        </div>
-    {/each}
+          &#10094;
+        </button>
+
+        <button
+          class="right-button absolute right-4 top-1/2 -translate-y-1/2 text-text text-4xl bg-black/30 hover:bg-black/50 transition-all p-4 rounded-full z-10"
+          on:click={handleNext}
+          aria-label="Следующий слайд"
+        >
+          &#10095;
+        </button>
+      </svelte:component>
+    </div>
+  {/each}
 </div>
--- src/lib/components/MainPage/Carousel/Items/Item3.svelte
+++ src/lib/components/MainPage/Carousel/Items/Item3.svelte
@@ -1,20 +1,22 @@
 <div class="main_container relative pt-5 mx-auto max-w-[1440px] pb-5">
-    <img class="absolute z-0 top-0 left-0 w-full h-full object-cover
-       transform scale-x-[-1] brightness-125" src="images/remontcars.jpg" alt="">
+  <img 
+    class="absolute z-0 top-0 left-0 w-full h-full object-cover brightness-125" 
+    src="images/remontcars.jpg" 
+    alt=""
+  >
       
-    <div class="main_container_block relative bg-white bg-opacity-50 p-6 mt-5 mb-5 rounded-lg mr-10 ml-14 md:ml-24 max-w-[790px] min-h-[350px] flex flex-col justify-center items-start  bg-gradient-to-r from-[rgba(255,255,255,0.8)] to-transparent">
-      <h1 class="main_text_bold font-bold text-[45px] text-third m-0 leading-[56px] xs:leading-[40px] xs:text-[30px] pb-5 ">График работы в праздничные дни:</h1>
+  <div class="main_container_block relative bg-white bg-opacity-50 p-6 mt-5 mb-5 rounded-lg mr-10 ml-14 md:ml-24 max-w-[790px] min-h-[350px] flex flex-col justify-center items-start bg-gradient-to-r from-[rgba(255,255,255,0.8)] to-transparent">
+    <h1 class="main_text_bold font-bold text-[45px] text-third m-0 leading-[56px] xs:leading-[40px] xs:text-[30px] pb-5">
+      График работы в праздничные дни:
+    </h1>
       
-     <ul class="main_text_second max-w-[590px] text-lg text-primary font-bold space-y-1">
-    <li>01.01 - выходной</li>
-    <li>23.02 - 10:00 до 18:00 </li>
-    <li>08.03 - 9:00 до 20:00</li>
-    <li>01.05 до 9:00 до 20:00 - выходной</li>
-  </ul>
-      
-    </div>
-  
-   <slot/>
-  
+    <ul class="main_text_second max-w-[590px] text-lg text-primary font-bold space-y-1">
+      <li>01.01 - выходной</li>
+      <li>23.02 - 10:00 до 18:00</li>
+      <li>08.03 - 9:00 до 20:00</li>
+      <li>01.05 - выходной</li>
+    </ul>
   </div>
   
+  <slot/>
+</div>
